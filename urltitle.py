698a011e602361f52b94937526df28746fdcaec3
Krzysztof Lasocki <krz.lasocki@gmail.com>
2022-03-09 15:03:35 +0100

    Fixup bug in UrlTitle

--- urltitle.py
+++ urltitle.py
@@ -10,7 +10,7 @@ def UrlTitle(data):
         if not url:
             return -1
         rq = urlopen(url.group(0), timeout=4)
-        title = re.findall("<title>(.*)</title>", rq.read())
+        title = re.findall("<title>(.*)</title>", rq.read().decode('utf-8'))
         if title == []:
                 return -2
         title = re.sub("[\n\r\t]", "", title[0])
@@ -20,7 +20,7 @@ def UrlTitle(data):
 
 
 if __name__ == "__main__":
-    UrlTitle(b":polprog!~ath0@www.polprog.net PRIVMSG #___# :test url http://example.org test123\r\n")
+    print(UrlTitle(":polprog!~ath0@www.polprog.net PRIVMSG #___# :test url http://example.org test123\r\n"))
     print(UrlTitle("https://cnn.com/"))
     print(UrlTitle("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
         
